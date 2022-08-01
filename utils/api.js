4e2537443b2ff0dba8aa642b1d4e9e8af94a38cf
Betelhem123Girma <betelhemgg4@gmail.com>
2022-08-01 15:53:45 +0300

    Api url updated

--- utils/api.js
+++ utils/api.js
@@ -1,3 +1,3 @@
 import axios from 'axios';
 export const register = (data) =>
-  axios.post('http://localhost:5001/register', data).then((res) => res.data);
+  axios.post('http://143.198.190.167:3000/register', data).then((res) => res.data);
